1c55c6ca7ae6abbdc8e974347345ac7670dadabd
Wouter Vroege <wouter@woutervroege.nl>
2022-01-26 20:16:23 +0100

    granulize tags, words and characters independently
    Signed-off-by: Wouter Vroege <wouter@woutervroege.nl>

--- src/granulize.js
+++ src/granulize.js
@@ -1,14 +1,15 @@
 export const granulize = (html, options={}) => {
   const config = {...{
-    granularity: 'word',
-    attribute: 'data-grain',
+    tags: true,
+    words: true,
+    characters: true,
     tagId: 'tag',
     wordId: 'word',
     characterId: 'char',
+    attribute: 'data-grain',
     indexTags: true,
     indexWords: true,
     indexCharacters: true,
-    clauseId: 'clause'
   }, ...options};
   let granulizedHTML = granulizeHTML(html, config);
   return granulizedHTML; 
@@ -16,23 +17,21 @@ export const granulize = (html, options={}) => {
 
 function granulizeHTML(text, cfg) {
   const tag = document.createElement('div');
-  tag.innerHTML = text;
+  tag.innerHTML = text.replace(/\s+/g, ' ').replace(/> </g, '><').trim();
 
   let html = parseTags(tag, null, cfg);
-  if(cfg.indexTags) html = indexGrains(html, cfg, 'tagId');
-  if(cfg.indexWords) html = indexGrains(html, cfg, 'wordId');
-  if(cfg.indexCharacters) html = indexGrains(html, cfg, 'characterId');
+  if(cfg.tags === true && cfg.indexTags) html = indexGrains(html, cfg, 'tagId');
+  if(cfg.words === true && cfg.indexWords) html = indexGrains(html, cfg, 'wordId');
+  if(cfg.characters === true && cfg.indexCharacters) html = indexGrains(html, cfg, 'characterId');
   return html;
 }
 
 function indexGrains(html, cfg, grainId) {
   const elem = document.createElement('div');
   elem.innerHTML = html;
-
   elem.querySelectorAll(`[${cfg.attribute}~="${cfg[grainId]}"]`).forEach((item, i) => {
     item.style.setProperty(`--${cfg[grainId]}-index`, `${i}`);
   })  
-
   return elem.innerHTML;
 }
 
@@ -40,7 +39,7 @@ function parseTags(tag, tagName, cfg) {
   const children = Array.from(tag.childNodes);
   const html = children.map(child => parseTag(child, cfg));
   if(!tagName) return html.join('');
-  else tag.setAttribute(cfg.attribute, `${cfg.tagId} ${cfg.tagId}-${tagName}`);
+  else if(cfg.tags === true) tag.setAttribute(cfg.attribute, `${cfg.tagId} ${cfg.tagId}-${tagName}`);
   tag.innerHTML = html.join('');
   return tag.outerHTML;
 }
@@ -51,6 +50,7 @@ function parseTag(tag, cfg) {
 }
 
 function parseWords(text, cfg) {
+  if(cfg.words !== true) return parseChars(text, cfg);
   const words = text.split(/\s/).map((word) => {
     const encodedWord = encodeURIComponent(word);
     return encodedWord.length === 0 ? '' : /*html*/`<span ${cfg.attribute}="${cfg.wordId} ${cfg.wordId}-${encodedWord}">${parseChars(word, cfg)}</span>`;
@@ -59,7 +59,7 @@ function parseWords(text, cfg) {
 }
 
 function parseChars(word, cfg) {
-  if(cfg.granularity !== 'character') return word;
+  if(cfg.characters !== true) return word;
   const chars = word.split(/\.*?/g).map((char) => /*html*/`<span ${cfg.attribute}="${cfg.characterId} ${cfg.characterId}-${encodeURIComponent(char)}">${char}</span>`);
   return chars.join('');
 }
